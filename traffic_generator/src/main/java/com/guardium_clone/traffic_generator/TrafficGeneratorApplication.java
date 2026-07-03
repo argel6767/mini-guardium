da25e0192fa29e0840f73585e1680b56c3afe9e4
argel6767 <argel6767@gmail.com>
2026-07-02 20:35:39 -0400

    add traffic-generator init

---
+++ traffic_generator/src/main/java/com/guardium_clone/traffic_generator/TrafficGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.guardium_clone.traffic_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficGeneratorApplication.class, args);
+	}
+
+}
---
+++ traffic_generator/src/test/java/com/guardium_clone/traffic_generator/TrafficGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guardium_clone.traffic_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
